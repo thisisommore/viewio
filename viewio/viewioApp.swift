31c2e69079cca6cfd5c581d15293f00be8542095
thisisommore <ommore501@gmail.com>
2026-07-12 23:36:39 +0530

    Initial Commit

---
+++ viewio/viewioApp.swift
@@ -0,0 +1,17 @@
+//
+//  viewioApp.swift
+//  viewio
+//
+//  Created by Om More on 12/07/26.
+//
+
+import SwiftUI
+
+@main
+struct viewioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ viewioTests/viewioTests.swift
@@ -0,0 +1,17 @@
+//
+//  viewioTests.swift
+//  viewioTests
+//
+//  Created by Om More on 12/07/26.
+//
+
+import Testing
+@testable import viewio
+
+struct viewioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
